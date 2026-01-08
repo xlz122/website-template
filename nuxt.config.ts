de794b9353c24f8fd425067c39c9336be90f6683
xlz122 <122874722@qq.com>
2026-01-08 09:33:34 +0800

    update example

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,6 +7,7 @@ export default defineNuxtConfig({
       title: 'website',
       link: [
         { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: 'stylesheet', type: 'text/css', href: '/styles/global.css' },
         {
           rel: 'preload',
           as: 'font',
@@ -24,6 +25,17 @@ export default defineNuxtConfig({
       ],
     },
   },
+  postcss: {
+    plugins: {
+      'postcss-mobile-forever': {
+        viewportWidth: 750,
+        mobileUnit: 'vw',
+        maxDisplayWidth: 750,
+        propList: ['*'],
+        selectorBlackList: [],
+      },
+    },
+  },
   devServer: {
     host: '0.0.0.0',
     port: 3000,
@@ -39,7 +51,7 @@ export default defineNuxtConfig({
         proxy: 'http://localhost:9006/**',
       },
       '/prod-api/**': {
-        proxy: 'http://127.0.0.1:9006/**',
+        proxy: 'http://localhost:9006/**',
       },
     },
   },
