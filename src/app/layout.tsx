47e8a9c7aa09944fae7eab2a69c8c23edc827344
xlz122 <122874722@qq.com>
2026-01-26 12:31:17 +0800

    update example

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,27 +2,22 @@ import type { Metadata } from 'next';
 import localFont from 'next/font/local';
 import Header from '../components/header/Header';
 import Footer from '../components/footer/Footer';
-import './globals.css';
+import '../../public/styles/globals.css';
 
 export const metadata: Metadata = {
   title: 'website',
   description: 'website built with next.js',
 };
 
-const InterSemiBold = localFont({
-  src: '../../public/fonts/Inter-SemiBold.woff2',
-  variable: '--font-inter-semibold',
-});
-
-const InterRegular = localFont({
-  src: '../../public/fonts/Inter-Regular.woff2',
-  variable: '--font-inter-regular',
+const PingFangMedium = localFont({
+  src: '../../public/fonts/PingFang-Medium.woff2',
+  variable: '--font-pingfang',
 });
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={`${InterSemiBold.variable} ${InterRegular.variable}`}>
+      <body className={`${PingFangMedium.variable}`}>
         <Header />
         {children}
         <Footer />
